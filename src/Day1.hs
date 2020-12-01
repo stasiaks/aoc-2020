b324d3e5b2cb0950a88f4a0f5515f83e27d8bcbc
Sebastian Stasiak <12955338+stasiaks@users.noreply.github.com>
2020-12-01 21:09:39 +0100

    Day 1 Part 1

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,7 +1,15 @@
 module Main where
+import Data.List
 
 main :: IO ()
 main = do
-  --contents <- getContents
-  --let input = map read (lines contents)
-  print "Part 1:"
+  contents <- getContents
+  let input = map read (lines contents)
+  putStrLn "Part 1:"
+  print $ part1 input
+
+part1 :: [Integer] -> Integer
+part1 xs = head [x*y | (x, y) <- allPairs $ sort xs, x+y == 2020]
+
+allPairs :: [Integer] -> [(Integer, Integer)]
+allPairs xs = [(x,y) | (x:ys) <- tails xs, y <- ys]
