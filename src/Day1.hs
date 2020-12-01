c8fbeb113d8e77b5bf8b6e7d97c17055df044444
Sebastian Stasiak <12955338+stasiaks@users.noreply.github.com>
2020-12-01 21:17:59 +0100

    Day 1 Part 2

--- src/Day1.hs
+++ src/Day1.hs
@@ -7,9 +7,17 @@ main = do
   let input = map read (lines contents)
   putStrLn "Part 1:"
   print $ part1 input
+  putStrLn "Part 2:"
+  print $ part2 input
 
 part1 :: [Integer] -> Integer
 part1 xs = head [x*y | (x, y) <- allPairs $ sort xs, x+y == 2020]
 
+part2 :: [Integer] -> Integer
+part2 xs = head [x*y*z | (x, y, z) <- allTrios xs, x+y+z == 2020]
+
 allPairs :: [Integer] -> [(Integer, Integer)]
 allPairs xs = [(x,y) | (x:ys) <- tails xs, y <- ys]
+
+allTrios :: [Integer] -> [(Integer, Integer, Integer)]
+allTrios xs = [(x,y,z) | (x:ys) <- tails xs, (y, z) <- allPairs ys]
