05344592c1eb1a9009b8839032143d963cd88825
Sebastian Stasiak <12955338+stasiaks@users.noreply.github.com>
2020-12-02 20:22:20 +0100

    Day 2 Part 2

--- src/Day2.hs
+++ src/Day2.hs
@@ -1,15 +1,15 @@
 module Main where
 import Data.Char (isDigit, isSpace)
 import Data.List.Split (splitOn)
-
+import Data.Algebra.Boolean (xor)
 
 trim :: String -> String
 trim = f . f
    where f = reverse . dropWhile isSpace
 
 data Policy = Policy
-    { minOccurences :: Int
-    , maxOccurences :: Int
+    { firstNumber :: Int
+    , secondNumber :: Int
     , char :: Char } deriving (Show)
 
 data PasswordEntry = PasswordEntry
@@ -18,13 +18,13 @@ data PasswordEntry = PasswordEntry
 
 instance Read Policy where
     readsPrec _ input =
-        let (minString, rest1) = span isDigit input
-            minValue = read minString :: Int
+        let (fstString, rest1) = span isDigit input
+            fstValue = read fstString :: Int
             (_:rest2) = rest1
-            (maxString, rest3) = span isDigit rest2
-            maxValue = read maxString :: Int
+            (sndString, rest3) = span isDigit rest2
+            sndValue = read sndString :: Int
             (_:char:rest4) = rest3
-            in [(Policy {minOccurences=minValue, maxOccurences=maxValue, char=char}, rest4)]
+            in [(Policy {firstNumber=fstValue, secondNumber=sndValue, char=char}, rest4)]
 
 instance Read PasswordEntry where
     readsPrec _ input =
@@ -39,17 +39,24 @@ main = do
   let input = map read (lines contents) :: [PasswordEntry]
   putStrLn "Part 1:"
   print $ part1 input
-  --putStrLn "Part 2:"
-  --print $ part2 input
+  putStrLn "Part 2:"
+  print $ part2 input
 
 part1 :: [PasswordEntry] -> Int
-part1 xs = length $ filter validate xs
+part1 xs = length $ filter (validate runPolicy1) xs
 
-validate :: PasswordEntry -> Bool
-validate PasswordEntry{policy = a, password = b} = runPolicy a b
+part2 :: [PasswordEntry] -> Int
+part2 xs = length $ filter (validate runPolicy2) xs
 
-runPolicy :: Policy -> String -> Bool
-runPolicy policy password = 
+validate :: (Policy -> String -> Bool) -> PasswordEntry -> Bool
+validate rule PasswordEntry{policy = a, password = b} = rule a b
+
+runPolicy1 :: Policy -> String -> Bool
+runPolicy1 policy password = 
     let occurences = length $ filter (==char policy) password
-        in minOccurences policy <= occurences && maxOccurences policy >= occurences
+        in firstNumber policy <= occurences && secondNumber policy >= occurences
+
+runPolicy2 :: Policy -> String -> Bool
+runPolicy2 Policy{firstNumber = a, secondNumber = b, char = c} password =
+    password!!(a-1) == c `xor` password!!(b-1) == c
 
