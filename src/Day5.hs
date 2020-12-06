5d8acb46f468aef278c450219c9d27df8fa022a0
Sebastian Stasiak <arionw@live.com>
2020-12-06 01:28:41 +0100

    Day 5 Part 2

--- src/Day5.hs
+++ src/Day5.hs
@@ -1,6 +1,7 @@
 module Main where
 import Prelude hiding (Left, Right) -- Don't comment, I'm too lazy to make different names
 import Data.List.Split (chunksOf)
+import Data.Sort (sort)
 
 data Side = Front | Back | Left | Right deriving Show
 
@@ -40,15 +41,28 @@ main = do
   let input = map (map read . chunksOf 1) $ lines contents :: [BoardingPass]
   putStrLn "Part 1:"
   print $ part1 input
-  --putStrLn "Part 2:"
-  --print $ part2 input
+  putStrLn "Part 2:"
+  print $ part2 input
 
 part1 :: [BoardingPass] -> Int
 part1 xs = maximum $ map (seatId . getSeat . f) xs
     where f = foldl (</>) (Range2D (Range (0, 127), Range (0, 7)))
+
+part2 :: [BoardingPass] -> Int
+part2 xs = (+1) $ last $ head $ groupWhen (\a b -> b == (a+1)) $ sort ids
+    where f = foldl (</>) (Range2D (Range (0, 127), Range (0, 7)))
+          ids = map (seatId . getSeat . f) xs
 
 getSeat :: Range2D -> (Int, Int)
 getSeat (Range2D (Range (a, _), Range (b, _))) = (a, b) -- As optimistic as possible, I'm tired today
 
 seatId :: (Int, Int) -> Int
 seatId (a, b) = a*8  + b
+
+-- Ripped from https://gitlab.haskell.org/ghc/ghc/-/issues/1408
+groupWhen :: (a -> a -> Bool) -> [a] -> [[a]]
+groupWhen _ []    = []
+groupWhen _ [a]   = [[a]]
+groupWhen f (a:l) = if f a (head c) then (a:c):r else [a]:c:r
+  where (c:r) = groupWhen f l
+
