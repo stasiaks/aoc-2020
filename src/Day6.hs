3c9cc97cfc175649efa1b6ea7d61991b0e4ef095
Sebastian Stasiak <arionw@live.com>
2020-12-06 19:19:46 +0100

    Day 6 Part 2

--- src/Day6.hs
+++ src/Day6.hs
@@ -1,5 +1,5 @@
 module Main where
-import Data.List (groupBy, nub)
+import Data.List (groupBy, nub, intersect)
 
 type Answers = [Char]
 type Group = [Answers]
@@ -10,9 +10,15 @@ main = do
   let input = paragraphs $ lines contents :: [Group]
   putStrLn "Part 1:"
   print $ part1 input
+  putStrLn "Part 2:"
+  print $ part2 input
 
 part1 :: [Group] -> Int
 part1 xs = sum $ map (length . nub . concat) xs
 
+part2 :: [Group] -> Int
+part2 xs = sum $ map (length . foldl1 intersect) xs
+
 paragraphs :: [String] -> [[String]]
 paragraphs = map (filter $ not . null) . groupBy (const $ not . null)
+
